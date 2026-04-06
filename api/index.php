6901f960218541b6e7b69ef537f0d8a0ce7cc8d8
diulll <badiulfikri24@gmail.com>
2026-04-07 01:24:52 +0700

    Fix Vercel read-only filesystem error

--- api/index.php
+++ api/index.php
@@ -1,4 +1,14 @@
 <?php
 
-// Arahkan ke titik masuk utama Laravel.
+// 1. Buat struktur folder di dalam /tmp milik Vercel
+$compiledViewPath = '/tmp/storage/framework/views';
+if (!file_exists($compiledViewPath)) {
+    mkdir($compiledViewPath, 0777, true);
+}
+
+// 2. Paksa Laravel menggunakan folder /tmp tersebut
+$_ENV['VIEW_COMPILED_PATH'] = $compiledViewPath;
+putenv('VIEW_COMPILED_PATH=' . $compiledViewPath);
+
+// 3. Lanjutkan ke aplikasi utama
 require __DIR__ . '/../public/index.php';
